b793a4abdb026e63b2a610f270bc44dda72f7560
Dumitru.Robu <orlando001@yandex.ru>
2023-09-13 14:04:06 +0300

    noi modificari 14:03, data 13/09/2023

--- app/Http/Controllers/AllControllers/StoreReviewController.php
+++ app/Http/Controllers/AllControllers/StoreReviewController.php
@@ -30,7 +30,12 @@ class StoreReviewController extends BaseController
             "semnatura"=>$semnaturaNume
         ]);
 
-        $carti = ModelCarti::all();
-        return view("/main", compact("carti"));
+//        $carti = ModelCarti::all();
+//        return view("/main", compact("carti"));
+
+          $book = ModelCarti::where("id", $cartea)->first();
+          $genulCartii = $book->genre->gen;
+          $reviews = ModelReviews::where("carte_id", $book->id)->get();
+          return view("/book", compact("book", "genulCartii", "reviews"));
     }
 }
